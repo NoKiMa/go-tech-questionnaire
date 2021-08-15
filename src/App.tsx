b7b3882e2803df1215d715c4c3f0e86a75d3881e
NoKi <novak.kirill@gmail.com>
2021-08-16 00:17:36 +0300

    creat style and UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import HeaderComponent from './components/HeaderComponent';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-       
+      <div className="container">
+        <HeaderComponent/>
+      </div>
     </div>
   );
 }
