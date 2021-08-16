f87a17e44f72541ae0f5592e8a36c41b6cdf2beb
NoKi <novak.kirill@gmail.com>
2021-08-16 23:25:22 +0300

    fix UI

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -5,7 +5,7 @@ const HeaderComponent = () =>{
   return (
     <div className="header_container">
         <div className="decoration"/>
-        <p className="title"> GoTech Qustionnere</p>
+        <p className="title"> GoTech Qustionnaire</p>
         <p className="subtitle"> Show me what you got</p>
         <p className="required"> *Required </p>
     </div>
